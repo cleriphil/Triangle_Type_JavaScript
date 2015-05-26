531a06d7c803d295126b15643329a353430b4e2e
Cory Spitzer & cleri philiastides <student@epicodus.com>
2015-05-26 11:14:14 -0700

    Identify isosceles triangles

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,10 @@ var triangleType = function(side1, side2, side3) {
   var sortedSides = sides.sort(numOrdA);
   if (sortedSides[2] > sortedSides[0] + sortedSides[1]) {
     return false;
-  } else {
+  } else if ((side1 === side2) && (side2 === side3)) {
     return "equilateral"
+  } else if (side1 === side2 || (side2 === side3 || side1 === side3)) {
+    return "isosceles"
   };
 };
 
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,12 @@ describe("triangleType", function() {
   it("is equilateral if all three sides are equal", function() {
     expect(triangleType(5,5,5)).to.equal("equilateral");
   });
+
+  it("is not equilateral if all three sides are not equal", function() {
+    expect(triangleType(5,5,6)).to.not.equal("equilateral");
+  });
+
+  it("is an isosceles triangle if exactly 2 sides are equal", function() {
+    expect(triangleType(6,6,4)).to.equal("isosceles");
+  });
 });
